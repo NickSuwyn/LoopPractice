d30ab3546fec3e91d08a631b58758239e414e7ec
Nick Suwyn <nicholas_suwyn@suwynitsolutions.com>
2020-04-07 19:24:48 -0700

    first commit

---
+++ src/Loops.java
@@ -0,0 +1,27 @@
+
+public class Loops {
+	
+	public static void main(String[] args) {
+		
+		int numberOfItems = 29;
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			
+		}
+		
+		String option = "";
+		
+		while (!option.equals("exit")) {
+			
+		}
+		
+		int numOfHeads = 0;
+		boolean isHeads = false;
+		
+		do (isHeads = flipCoin()) {
+			
+		} while (isHeads);
+		
+	}
+
+}
